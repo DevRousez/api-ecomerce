8fdd39aefd93cf16f47475f2d73f9620a484cc95
Aymer Ross <45677266+DevRousez@users.noreply.github.com>
2025-07-14 13:47:56 -0600

    Cambios reviews

--- Dtos/AccountsDtocs.cs
+++ Dtos/AccountsDtocs.cs
@@ -29,4 +29,30 @@ namespace Api_comerce.Dtos
 
              
     }
+    public class AccountDto
+    {
+        public int Id { get; set; }
+        public int AccountTypeId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string? GoogleId { get; set; }
+        public string? Picture { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+
+        public AccountsTypes AccountsTypes { get; set; }
+
+
+    }
+
 }
--- Services/ProductosComentarios/IProductosComentariosService.cs
+++ Services/ProductosComentarios/IProductosComentariosService.cs
@@ -4,9 +4,10 @@ namespace Api_comerce.Services.ProductosComentarios
 {
     public interface IProductosComentariosService
     {
-        Task<List<ProductosComentariosDTO>> GetAllAsync();
-        Task<ProductosComentariosDTO?> GetByIdAsync(int? id,int? productoEmpaqueId);
-    
+        Task<List<ProductoConComentariosDTO>> GetAllAsync(int? idUser = null);
+        Task<ProductoConComentariosDTO?> GetComentarioFiltradoAsync(int? id,int? productoEmpaqueId, int? userId = null);
+        Task<List<ProductoConComentariosDTO>> GetByLimitAsync(int? limit = 10, int? userId=0);
+
         Task<ProductosComentariosDTO> CreateAsync(CrearComentarioDTO dto);
         Task<bool> UpdateAsync(int id, ProductosComentariosDTO dto);
         Task<bool> DeleteAsync(int id);
